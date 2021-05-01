c704dcfcba0102f4bd4e476013a09e041761e8b0
ElibertoSubias <eliberto.subias.ortega@gmail.com>
2021-04-30 21:21:04 -0600

    avances en house

--- App.js
+++ App.js
@@ -2,10 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location';
+import Weatherinfo from './componets/WeatherInfo';
+
+const WATHER_API_KEY = '81eda6665a3c88aa8830ddfe6f466bd5';
+const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?';
 
 export default function App() {
 
   const [errorMessage, setErrorMessage] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [unitSystem, setUnitSystem] = useState('metric');
 
   useEffect(() => {
     load();
@@ -22,21 +28,42 @@ export default function App() {
       const location = await Location.getCurrentPositionAsync();
 
       const {latitude, longitude} = location.coords;
-      alert(`Latitude : ${latitude}, Longitude : ${longitude}`);
+      const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitSystem}&appid=${WATHER_API_KEY}`;
+
+      const response = await fetch(weatherUrl);
+
+      const ressult = await response.json();
+
+      if (response.ok) {
+        setCurrentWeather(ressult);
+      } else {
+        setErrorMessage(ressult.message);
+      }
 
     } catch (error) {
 
-      
+      setErrorMessage(error.message);
       
     }
   }
 
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  if (currentWeather) {
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <View style={styles.main}>
+          <Weatherinfo currentWeather={currentWeather}/>
+        </View>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>{errorMessage}</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -46,4 +73,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  main: {
+    justifyContent: 'center',
+    flex: 1
+  }
 });
